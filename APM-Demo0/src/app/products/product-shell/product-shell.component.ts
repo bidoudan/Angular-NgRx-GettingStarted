579923e8403918ad96238daed7cf71382d3c8798
quran.fr09@gmail.com <quran.fr09@gmail.com>
2020-12-31 20:51:40 +0100

    Adding a State index.ts File to state products

--- APM-Demo0/src/app/products/product-shell/product-shell.component.ts
+++ APM-Demo0/src/app/products/product-shell/product-shell.component.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 
 import { Product } from '../product';
 import { Store } from '@ngrx/store';
-import { getCurrentProduct, getError, getProducts, getShowProductCode, State } from '../state/product.reducer';
+import { getCurrentProduct, getError, getProducts, getShowProductCode, State } from '../state';
 import * as ProductActions from '../state/product.actions';
 @Component({
   templateUrl: './product-shell.component.html'
--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,68 +1,19 @@
-import { createFeatureSelector, createReducer, createSelector, on } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
 import { Product } from "../product";
-import * as AppState from '../../state/app.state';
 import * as ProductActions from './product.actions';
-import { Action } from "rxjs/internal/scheduler/Action";
-import { sample } from "rxjs/operators";
-import { act } from "@ngrx/effects";
-export interface State extends AppState.State {
-    products: ProductState;
-}
-
 export interface ProductState {
     showProductCode: boolean;
     currentProductId: number | null;
     products: Product[];
     error: string;
 }
-
+    
 const initialState: ProductState = {
     showProductCode: true,
     currentProductId: null,
     products: [],
     error: ''
 }
-
-const getProductFeatureState = createFeatureSelector<ProductState>('products');
-
-export const getShowProductCode = createSelector(
-    getProductFeatureState,
-    state => state.showProductCode
-);
-
-export const getCurrentProductId = createSelector(
-    getProductFeatureState,
-    state => state.currentProductId
-)
-
-export const getCurrentProduct = createSelector(
-    getProductFeatureState,
-    getCurrentProductId,
-
-    (state, currentProductId) => {
-        if(currentProductId === 0) {
-            return {
-                id: 0,
-                productName: '',
-                productCode: 'New',
-                description: '',
-                starRating: 0
-            };
-        } else {
-            return currentProductId ? state.products.find(p =>p.id === currentProductId) : null;
-        }
-    }
-);
-
-export const getProducts = createSelector(
-    getProductFeatureState,
-    state => state.products
-);
-
-export const getError = createSelector(
-    getProductFeatureState,
-    state => state.error
-)
 
 export const productReducer = createReducer<ProductState>(
     initialState,
@@ -72,7 +23,7 @@ export const productReducer = createReducer<ProductState>(
             showProductCode: !state.showProductCode
         };
     }),
-
+    
     on(ProductActions.setCurrentProduct, (state, action): ProductState => {
         return {
             ...state,
